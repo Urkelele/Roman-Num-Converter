ee854cd3ac480a173f7147225307b1c7a9e34b24
Urko Aguillo <118058363+Urkelele@users.noreply.github.com>
2026-05-05 12:53:37 +0200

    Add ESLint configuration with custom rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,1 +1,35 @@
-
+// eslint.config.js
+module.exports = [
+    {
+        "rules": {
+            "no-cond-assign": [
+                "error",
+                "always"
+            ],
+            "indent": [
+                "error",
+                4
+            ],
+            "linebreak-style": [
+                "error",
+                "unix"
+            ],
+            "quotes": [
+                "error",
+                "single"
+            ],
+            "no-unused-vars": "error",
+            "semi": [
+                "error",
+                "always"
+            ],
+            "no-throw-literal": "error",
+            "prefer-const": "error",
+            "eqeqeq": [
+                "error",
+                "always"
+            ],
+            "no-var": "error"
+        }
+    }
+];
